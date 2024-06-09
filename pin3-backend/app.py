ee75f3c6c8b3c5054b5d1839cd9bed4d72fab0c3
brucevahldick <brucev.br@gmail.com>
2024-06-09 10:30:00 -0300

    backend

--- pin3-backend/app.py
+++ pin3-backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        "message": "Hello from Flask!"
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
